fc7a5a58358b9df64cd769b05ca2ef58fd565665
John DeFeo <jackd3243@gmail.com>
2021-11-07 21:26:07 -0500

    commit tree 42fafe157a25133496b837e03aaf31e30738dd9c

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(arrayWords, drivername){
+    let drivers= arrayWords.filter(x => drivername.toUpperCase() == x.toUpperCase());
+    return drivers
+}
+function fuzzyMatch(arrayWords, letters) {
+    let drivers = arrayWords.filter(x => x.indexOf(letters) === 0)
+    return drivers
+}
+function  matchName(arrayObjects,string){
+    let nameMatch = arrayObjects.filter(x => x.name === string )
+    return nameMatch
+}
